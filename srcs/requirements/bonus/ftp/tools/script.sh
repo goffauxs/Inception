618363a540922111962cc98c64873ed7e3c5cc20
goffauxs <goffauxs@gmail.com>
2022-06-02 16:01:55 +0200

    ftp working

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
 mkdir -p /var/run/vsftpd/empty
-groupadd ftp
-useradd -d /ftp -s /bin/bash -m -g ftp sgoffaux
-echo -e "123\n123" | passwd sgoffaux
-chown -R sgoffaux:ftp /ftp
-
+useradd -d /ftp -s /bin/bash -m -u 4321 $FTP_USER
+echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
+groupadd ftpgroup
+usermod -G ftpgroup $FTP_USER
+chown -R $FTP_USER:ftpgroup /ftp
+chmod 777 /ftp
+echo "$FTP_USER" | tee -a /etc/vsftpd.userlist
+exec vsftpd
