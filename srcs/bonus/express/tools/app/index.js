e02d5db0e6a2d4405064fc9f73dde5de5942f15b
Simon Goffaux <sgoffaux@student.s19.be>
2022-06-06 10:36:26 +0200

    change express port to 3000 for linux compatibility

--- srcs/bonus/express/tools/app/index.js
+++ srcs/bonus/express/tools/app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port  = process.env.port || 80;
+const port  = process.env.port || 3000;
 
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, '/index.html'));
